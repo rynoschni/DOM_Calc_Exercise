a92808878f3f6c3955d5fdc23235cfe79436543e
Eric Schorling <eric.schorling@yahoo.com>
2020-08-19 17:07:36 -0400

    no repeat values no switch statement

--- scripts/index.js
+++ scripts/index.js
@@ -9,14 +9,16 @@ const calcScreen = document.getElementById('input');
 let numbReg = /\d/;
 let equalPressed = false;
 let characterReg = /\D/
+let operatorArr = ["+","/","+","-"]
 
 numberButtonNode.forEach(function (button){
     button.addEventListener('click', function(e){
         event.preventDefault();
         inputArr = [...inputArr, button.value]
         console.log(inputArr)
-        if (calcScreen.value == 0){
+        if (calcScreen.value == 0 || equalPressed){
             calcScreen.value = button.value
+            equalPressed = false
         }
         else {calcScreen.value += button.value}
     })
@@ -63,27 +65,40 @@ equalButton.addEventListener('click',function(e){
     }
     operationArr = [...operationArr, Number(firstNum)]
 
-    for (let i = 0; i< operationArr.length; i++){
-        switch (operationArr[i]){
-            case "*":
-                operationArr.splice(i-1,3, operationArr[i-1] * operationArr[i+1]);
-                i=0
-                break;
-            case "/":
-                operationArr.splice(i-1,3, operationArr[i-1] / operationArr[i+1]);
-                i=0
-                break;
-            case "+":
-                console.log(operationArr[i])
-                operationArr.splice(i-1,3, operationArr[i-1] + operationArr[i+1]);
-                i=0
-                break;
-            case "-":
-                operationArr.splice(i-1,3, operationArr[i-1] - operationArr[i+1]);
-                i=0
-                break;
-        }
-        
+    let i = 0;
+
+    //console.log('happened1')
+    //for (let oper in operatorArr)
+    while (operationArr.indexOf("*") !== -1){
+        i = operationArr.indexOf("*")
+        operationArr.splice(i-1,3, operationArr[i-1] * operationArr[i+1]);
+        console.log('happened2')
+        console.log(operationArr)
     }
+
+    while (operationArr.indexOf("/") !== -1){
+        i = operationArr.indexOf("/")
+        operationArr.splice(i-1,3, operationArr[i-1] / operationArr[i+1]);
+        console.log('happened2')
+        console.log(operationArr)
+    }
+
+    while (operationArr.indexOf("+") !== -1){
+        i = operationArr.indexOf("+")
+        operationArr.splice(i-1,3, operationArr[i-1] + operationArr[i+1]);
+        console.log('happened2')
+        console.log(operationArr)
+    }
+
+    while (operationArr.indexOf("-") !== -1){
+        i = operationArr.indexOf("-")
+        operationArr.splice(i-1,3, operationArr[i-1] - operationArr[i+1]);
+        console.log('happened2')
+        console.log(operationArr)
+    }
+    equalPressed = true;
+    inputArr = []
+
+        
     calcScreen.value = operationArr
 });
